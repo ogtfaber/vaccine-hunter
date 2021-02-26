6c516f4bd2267a242f3eda46681bda888decd143
Onno Faber <onno@onnofaber.com>
2021-02-26 14:32:24 -0800

    - Removed email in favor of audible notifications - Focus on 1 state

--- vaccine-hunter.js
+++ vaccine-hunter.js
@@ -1,7 +1,6 @@
 const dotenv = require('dotenv')
 const cron = require('node-cron')
 const express = require('express')
-const nodemailer = require('nodemailer')
 const util = require('util')
 const exec = util.promisify(require('child_process').exec)
 
@@ -9,90 +8,30 @@ const exec = util.promisify(require('child_process').exec)
 const app = express()
 dotenv.config()
 
-const md_recipients = 'mike.endale@gmail.com'
-const va_recipients = 'mike.endale@gmail.com'
-
-// Create mail transporter.
-// Highly recommend to avoid using your personal email
-// account for this step. Quick solutions on the internet
-// may guide you towards lowering security measures.
-// Consider using a new separate developer account to
-// avoid any risk to your personal email account.
-const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST,
-  port: process.env.SMTP_PORT,
-  secure: true,
-  auth: {
-    user: process.env.SMTP_AUTH_USERNAME,
-    pass: process.env.SMTP_AUTH_PASSWORD,
-  },
-})
-
-const hunt = async (state) => {
-  if (!state) {
-    console.error('State is required')
-    return
-  }
-
+const hunt = async () => {
   try {
-    const { stdout, stderr } =
-      state === 'MD'
-        ? await exec('sh scripts/md-hunt.sh')
-        : await exec('sh scripts/va-hunt.sh')
-
+    const { stdout, stderr } = await exec('sh scripts/ca-hunt.sh')
     if (stdout) {
-      const data =
-        state === 'MD'
-          ? JSON.parse(stdout).responsePayloadData.data['MD']
-          : JSON.parse(stdout).responsePayloadData.data['VA']
-
-      return data
-        .map((p) => `City: ${p.city}, Total Available: ${p.totalAvailable} \n`)
-        .toString()
+      const data = JSON.parse(stdout).responsePayloadData.data['CA']
+      const availableCities = data.filter(d => d.status !== 'Fully Booked').map(d => d.city).join(', ')
+      return availableCities
     }
   } catch (err) {
     console.error(err)
   }
 }
 
-cron.schedule('*/15  * * * *', function () {
+cron.schedule('*/5  * * * *', function () {
   console.log('---------------------')
   console.log('Running Hunter')
-
-  // Check Maryland and email
-  hunt('MD').then((res) => {
-    let messageOptions = {
-      from: process.env.FROM,
-      to: process.env.MD_RECIPIENTS,
-      subject: 'Maryland CVS Vaccine Availability',
-      text: res,
+  
+  hunt().then((res) => {
+    console.log('Checked ', new Date().toISOString())
+    if (res) {
+      exec('say vaccine found in ' + res)
+    } else {
+      // exec('say no vaccine found yet')
     }
-
-    transporter.sendMail(messageOptions, function (error, info) {
-      if (error) {
-        throw error
-      } else {
-        console.log('Maryland email successfully sent!')
-      }
-    })
-  })
-
-  // Check Virginia and email
-  hunt('VA').then((res) => {
-    let messageOptions = {
-      from: process.env.FROM,
-      to: process.env.VA_RECIPIENTS,
-      subject: 'Virginia CVS Vaccine Availability',
-      text: res,
-    }
-
-    transporter.sendMail(messageOptions, function (error, info) {
-      if (error) {
-        throw error
-      } else {
-        console.log('Virginia email successfully sent!')
-      }
-    })
   })
 })
 
